aa8d3aaf3791933350e6e5e1c3b4ec982733ac25
Argian Ardi Prasetya <argianardi1435@gmail.com>
2023-02-25 03:18:07 +0700

    change app logo color

--- src/components/Header.js
+++ src/components/Header.js
@@ -3,12 +3,12 @@ import { BsCartFill } from "react-icons/bs";
 
 const Header = ({ cartCount, handleOpen }) => {
   return (
-    <div className="w-full h-20 bg-gray-100 py-2 px-10 fixed top-0 left-0 justify-end items-center flex">
+    <div className="w-full h-20 bg-gray-100 py-2 px-10 fixed top-0 left-0 justify-end items-center flex z-10">
       <div
         className="w-12 h-12 relative rounded-full bg-slate-200 flex items-center justify-center cursor-pointer"
         onClick={handleOpen}
       >
-        <BsCartFill />
+        <BsCartFill size={25} className="text-orange-500" />
         {cartCount?.length ? (
           <div className="absolute top-1 right-1 text-xs rounded-full bg-red-500 text-white px-1">
             {cartCount.length}
