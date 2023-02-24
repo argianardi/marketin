5a53967df1437751916f11587a44655c59f79dcb
Argian Ardi Prasetya <argianardi1435@gmail.com>
2023-02-25 00:20:04 +0700

    add header and product component

--- src/App.js
+++ src/App.js
@@ -1,9 +1,26 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
+import { useDispatch, useSelector } from "react-redux";
+import Header from "./components/Header";
+import Products from "./components/Products";
+import { addToCart, getProducts } from "./utils/redux/cart/productSlice";
 
 function App() {
+  const dispatch = useDispatch();
+  const { products, cart } = useSelector((state) => state.products);
+  const [isOpen, setIsOpen] = useState(false);
+  const handleOpen = () => {
+    setIsOpen(!isOpen);
+  };
+  const addtocart = (id) => dispatch(addToCart(id));
+
+  useEffect(() => {
+    dispatch(getProducts());
+  }, [dispatch]);
+
   return (
     <>
-      <h1 className="text-3xl font-bold underline">Hello world!</h1>
+      <Header cartCount={cart} handleOpen={handleOpen} />
+      <Products products={products} addToCart={addtocart} />
     </>
   );
 }
